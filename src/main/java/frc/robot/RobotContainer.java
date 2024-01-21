3f080a05aa5d4e42532d4ed1cdbae4b8acc9783e
Mochatitan <mochatitanstudios@gmail.com>
2024-01-21 13:48:28 -0500

    commented out example code
    error msg said it tried to find a txt file that didnt exist. so i made it not exist.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -80,51 +80,51 @@ public class RobotContainer {
 
         SmartDashboard.putData("BlueAllianceMiddleAuto", new PathPlannerAuto("BlueAllianceMiddleAuto"));
         // Add a button to run the example auto to SmartDashboard, this will also be in the auto chooser built above
-    SmartDashboard.putData("Example Auto", new PathPlannerAuto("Example Auto"));
-    // Add a button to run pathfinding commands to SmartDashboard
-    SmartDashboard.putData("Pathfind to Pickup Pos", AutoBuilder.pathfindToPose(
-      new Pose2d(14.0, 6.5, Rotation2d.fromDegrees(0)), 
-      new PathConstraints(
-        4.0, 4.0, 
-        Units.degreesToRadians(360), Units.degreesToRadians(540)
-      ), 
-      0, 
-      2.0
-    ));
-    SmartDashboard.putData("Pathfind to Scoring Pos", AutoBuilder.pathfindToPose(
-      new Pose2d(2.15, 3.0, Rotation2d.fromDegrees(180)), 
-      new PathConstraints(
-        4.0, 4.0, 
-        Units.degreesToRadians(360), Units.degreesToRadians(540)
-      ), 
-      0, 
-      0
-    ));
+    // SmartDashboard.putData("Example Auto", new PathPlannerAuto("Example Auto"));
+    // // Add a button to run pathfinding commands to SmartDashboard
+    // SmartDashboard.putData("Pathfind to Pickup Pos", AutoBuilder.pathfindToPose(
+    //   new Pose2d(14.0, 6.5, Rotation2d.fromDegrees(0)), 
+    //   new PathConstraints(
+    //     4.0, 4.0, 
+    //     Units.degreesToRadians(360), Units.degreesToRadians(540)
+    //   ), 
+    //   0, 
+    //   2.0
+    // ));
+    // SmartDashboard.putData("Pathfind to Scoring Pos", AutoBuilder.pathfindToPose(
+    //   new Pose2d(2.15, 3.0, Rotation2d.fromDegrees(180)), 
+    //   new PathConstraints(
+    //     4.0, 4.0, 
+    //     Units.degreesToRadians(360), Units.degreesToRadians(540)
+    //   ), 
+    //   0, 
+    //   0
+    // ));
 
-    // Add a button to SmartDashboard that will create and follow an on-the-fly path
-    // This example will simply move the robot 2m in the +X field direction
-    SmartDashboard.putData("On-the-fly path", Commands.runOnce(() -> {
-      Pose2d currentPose = swerveSubsystem.getPose();
+    // // Add a button to SmartDashboard that will create and follow an on-the-fly path
+    // // This example will simply move the robot 2m in the +X field direction
+    // SmartDashboard.putData("On-the-fly path", Commands.runOnce(() -> {
+    //   Pose2d currentPose = swerveSubsystem.getPose();
       
-      // The rotation component in these poses represents the direction of travel
-      Pose2d startPos = new Pose2d(currentPose.getTranslation(), new Rotation2d());
-      Pose2d endPos = new Pose2d(currentPose.getTranslation().plus(new Translation2d(2.0, 0.0)), new Rotation2d());
+    //   // The rotation component in these poses represents the direction of travel
+    //   Pose2d startPos = new Pose2d(currentPose.getTranslation(), new Rotation2d());
+    //   Pose2d endPos = new Pose2d(currentPose.getTranslation().plus(new Translation2d(2.0, 0.0)), new Rotation2d());
 
-      List<Translation2d> bezierPoints = PathPlannerPath.bezierFromPoses(startPos, endPos);
-      PathPlannerPath path = new PathPlannerPath(
-        bezierPoints, 
-        new PathConstraints(
-          4.0, 4.0, 
-          Units.degreesToRadians(360), Units.degreesToRadians(540)
-        ),  
-        new GoalEndState(0.0, currentPose.getRotation())
-      );
+    //   List<Translation2d> bezierPoints = PathPlannerPath.bezierFromPoses(startPos, endPos);
+    //   PathPlannerPath path = new PathPlannerPath(
+    //     bezierPoints, 
+    //     new PathConstraints(
+    //       4.0, 4.0, 
+    //       Units.degreesToRadians(360), Units.degreesToRadians(540)
+    //     ),  
+    //     new GoalEndState(0.0, currentPose.getRotation())
+    //   );
 
-      // Prevent this path from being flipped on the red alliance, since the given positions are already correct
-      path.preventFlipping = true;
+    //   // Prevent this path from being flipped on the red alliance, since the given positions are already correct
+    //   path.preventFlipping = true;
 
-      AutoBuilder.followPath(path).schedule();
-    }));
+    //   AutoBuilder.followPath(path).schedule();
+    // }));
     }
     
 }
