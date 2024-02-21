09f5f1a86e04cb3eaeae6c07f84fe28b5eb7316b
IcedJavaRobotics <icedjavarobotics@gmail.com>
2024-02-20 20:34:27 -0500

    added intake pidcontroller

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -7,6 +7,7 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -17,6 +18,10 @@ public class IntakeSubsystem extends SubsystemBase {
   public CANSparkMax intakeMotor = new CANSparkMax(IntakeConstants.SPARK_MAX_ID, MotorType.kBrushless);
 
   public DigitalInput intakeLimitSwitch = new DigitalInput(IntakeConstants.LIMIT_SWITCH_ID);
+
+  public PIDController intakePidController;
+
+  private double pidCalculations;
 
   /** Creates a new IntakeSubsystem. */
   public IntakeSubsystem() {
