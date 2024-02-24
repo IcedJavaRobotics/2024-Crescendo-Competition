9f3d1b1c68418b7cf30b5fc12c1ae4c0a5caf710
IcedJavaRobotics <icedjavarobotics@gmail.com>
2024-02-24 11:31:43 -0500

    Tried thigs with triggers

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,12 +10,16 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.RumbleType;
+import edu.wpi.first.wpilibj.XboxController.Axis;
+import edu.wpi.first.wpilibj.event.EventLoop;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.OIConstants;
 import frc.robot.commands.shooter.NoteShootCommand;
 import frc.robot.commands.shooter.ShooterOutCommand;
@@ -38,6 +42,8 @@ import frc.robot.subsystems.PneumaticSubsystem;
 import frc.robot.subsystems.SwerveSubsystem;
 import frc.robot.commands.FlasherCommand;
 import frc.robot.subsystems.LimelightSubsystem;
+
+import java.util.function.BooleanSupplier;
 
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
@@ -65,8 +71,16 @@ public class RobotContainer {
     //private final SendableChooser<Command> autoChooser; 
 
     private final XboxController driverController = new XboxController(OIConstants.DRIVER_CONTROLLER_PORT);
+    private final CommandXboxController driverController2 = new CommandXboxController(OIConstants.DRIVER_CONTROLLER_PORT);
     private final XboxController auxController = new XboxController(OIConstants.AUX_CONTROLLER_PORT);
     private final Joystick driverStation = new Joystick(OIConstants.DRIVER_STATION_PORT);
+
+    public Trigger rTrigger = auxController.getRightTriggerAxis();
+
+    public Trigger auxRTrigger = new Trigger(new BooleanSupplier bool = (driverController2.getRightTriggerAxis()>0.2));
+    BooleanSupplier boole = new BooleanSupplier(){
+        
+    }
 
     public RobotContainer() {
         swerveSubsystem.setDefaultCommand(new SwerveJoystickCmd(
@@ -121,9 +135,6 @@ public class RobotContainer {
  
 
         new JoystickButton(auxController, XboxController.Button.kA.value)
-                .whileTrue(new NoteShootCommand(shooterSubsystem, rollerSubsystem));
-
-        new JoystickAxis(auxController, (XboxController.Axis.kRightTrigger.value > 0.2) ? true : false)
                 .whileTrue(new NoteShootCommand(shooterSubsystem, rollerSubsystem));
 
         // new JoystickButton(auxController, XboxController.Button.kRightBumper.value)
