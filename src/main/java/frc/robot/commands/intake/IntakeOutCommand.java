ea720b8cfc3cf44ae7add2ccd695ceb5370c12d5
Louis-Fragapane <156540284+Louis-Fragapane@users.noreply.github.com>
2024-02-27 11:22:15 -0500

    Out

--- src/main/java/frc/robot/commands/intake/IntakeOutCommand.java
+++ src/main/java/frc/robot/commands/intake/IntakeOutCommand.java
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.IntakeSubsystem;
 
 public class IntakeOutCommand extends Command {
-  /** Creates a new IntakeDownCommand. */
+  /** Creates a new IntakOutCommand. */
   
   private final IntakeSubsystem intakeSubsystem;
   
