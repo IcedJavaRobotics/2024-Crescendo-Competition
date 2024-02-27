b0daac25eb2348d8a0a10d06274623e40836f483
Louis-Fragapane <156540284+Louis-Fragapane@users.noreply.github.com>
2024-02-27 11:21:53 -0500

    Renamed to InCommand instead of up command

--- src/main/java/frc/robot/commands/intake/IntakeInCommand.java
+++ src/main/java/frc/robot/commands/intake/IntakeInCommand.java
@@ -7,12 +7,12 @@ package frc.robot.commands.intake;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.IntakeSubsystem;
 
-public class IntakeUpCommand extends Command {
-  /** Creates a new IntakeUpCommand. */
+public class IntakeInCommand extends Command {
+  /** Creates a new IntakeInCommand. */
 
   private final IntakeSubsystem intakeSubsystem;
 
-  public IntakeUpCommand(IntakeSubsystem subsystem) {
+  public IntakeInCommand(IntakeSubsystem subsystem) {
     intakeSubsystem = subsystem;
     addRequirements(intakeSubsystem);
     // Use addRequirements() here to declare subsystem dependencies.
