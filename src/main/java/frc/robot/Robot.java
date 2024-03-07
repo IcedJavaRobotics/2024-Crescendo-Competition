fe3af8a0b2980a349890dc337bee05e4f463937b
IcedJavaRobotics <icedjavarobotics@gmail.com>
2024-03-06 20:12:58 -0500

    chaging auto paths

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -20,7 +20,7 @@ public class Robot extends TimedRobot {
   private RobotContainer m_robotContainer;
 
   /**
-   * This function is run when the robot is first started up and should be used for any
+   * This function is run when the robot is first started  up and should be used for any
    * initialization code.
    */
   @Override
